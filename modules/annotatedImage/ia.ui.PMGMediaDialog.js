835d899ee50539839e69a441330fec2f1bf400a7
Pierre Boutet <boutetpierre@yahoo.fr>
2019-04-17 14:58:24 +0200

    default value for image insertion

--- modules/annotatedImage/ia.ui.PMGMediaDialog.js
+++ modules/annotatedImage/ia.ui.PMGMediaDialog.js
@@ -749,8 +749,8 @@ ve.ui.PMGMediaDialog.prototype.confirmSelectedImage = function () {
 					width: info.thumbwidth,
 					height: info.thumbheight,
 					mediaType: info.mediatype,
-					type: 'thumb',
-					align: 'none',
+					type: 'frameless',
+					align: 'center',
 					defaultSize: true
 				},
 				this.getFragment().getDocument()
